03416006aa5a0050547d47c1c63963ecf0c2e4f9
Maxime <132160016+ShikiTsugu2@users.noreply.github.com>
2025-04-09 15:38:12 +0200

    Add files via upload

---
+++ utils/text_processing.py
@@ -0,0 +1,20 @@
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('punkt')
+
+stop_words = set(stopwords.words("english"))
+lemmatizer = WordNetLemmatizer()
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\d+', '', text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    tokens = nltk.word_tokenize(text)
+    tokens = [lemmatizer.lemmatize(w) for w in tokens if w not in stop_words]
+    return " ".join(tokens)
